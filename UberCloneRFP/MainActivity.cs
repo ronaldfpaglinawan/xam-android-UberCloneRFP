3ac6640ab7ced0fe5b1f1c2aa8144e8f286e354a
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-13 12:31:49 +1200

    added codes in MainActivity.cs and button in activity_main.xml to initialize and test the Firebase Database connection

--- UberCloneRFP/MainActivity.cs
+++ UberCloneRFP/MainActivity.cs
@@ -3,19 +3,60 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Firebase.Database;
+using Firebase;
 
 namespace UberCloneRFP
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        FirebaseDatabase database;
+        Button btntestConnection;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            btntestConnection = (Button)FindViewById(Resource.Id.mybutton);
+            btntestConnection.Click += BtntestConnection_Click;
         }
+
+        private void BtntestConnection_Click(object sender, System.EventArgs e)
+        {
+            InitialDatabase();
+        }
+
+        void InitialDatabase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("uberclone-6596f")
+                    .SetApiKey("AIzaSyAXqLpF_JVINHqrS74X_r6uMG8wW5EZlzs")
+                    .SetDatabaseUrl("https://uberclone-6596f.firebaseio.com")
+                    .SetStorageBucket("uberclone-6596f.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+
+            DatabaseReference dbref = database.GetReference("UserSupport");
+            dbref.SetValue("RFP_Ticket1");
+
+            Toast.MakeText(this, "Completed", ToastLength.Short).Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
