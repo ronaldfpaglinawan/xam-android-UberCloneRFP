57fe352deee91c22cbb4caec588af9ee5b203e69
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-17 11:17:36 +1200

    added RegistrationActivity.cs and register.xml

--- UberCloneRFP/Activities/RegistrationActivity.cs
+++ UberCloneRFP/Activities/RegistrationActivity.cs
@@ -7,22 +7,20 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
-using Android.Support.V7.App;
-using Firebase.Database;
-using Firebase;
 
 namespace UberCloneRFP.Activities
 {
-    [Activity(Label = "@string/app_name", Theme ="@style/UberTheme", MainLauncher = false)]
-    public class LoginActivity : Activity
+    [Activity(Label = "@string/app_name", Theme = "@style/UberTheme", MainLauncher = true)]
+    public class RegistrationActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            SetContentView(Resource.Layout.login);
+            SetContentView(Resource.Layout.register);
         }
     }
 }
