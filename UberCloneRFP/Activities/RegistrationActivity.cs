85e58954083506641d2625ed4169071cb4310205
Ronald Fornis Paglinawan <ronaldpaglinawan@yahoo.com>
2020-04-22 18:04:22 +1200

    implemented the User Registration using Firebase Authentication

--- UberCloneRFP/Activities/RegistrationActivity.cs
+++ UberCloneRFP/Activities/RegistrationActivity.cs
@@ -7,20 +7,126 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
+using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase;
+using Android.Gms.Tasks;
+using UberCloneRFP.EventListeners;
 
 namespace UberCloneRFP.Activities
 {
     [Activity(Label = "@string/app_name", Theme = "@style/UberTheme", MainLauncher = false)]
     public class RegistrationActivity : AppCompatActivity
     {
+        TextInputLayout fullNameText;
+        TextInputLayout phoneText;
+        TextInputLayout emailText;
+        TextInputLayout passwordText;
+        Button registerButton;
+        CoordinatorLayout rootView;
+
+        FirebaseAuth mAuth;
+        FirebaseDatabase database;
+        TaskCompletionListener taskCompletionListener = new TaskCompletionListener();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.register);
+
+            InitialFirebase();
+            mAuth = FirebaseAuth.Instance;
+            ConnectControl();
+        }
+
+        void ConnectControl()
+        {
+            fullNameText = (TextInputLayout)FindViewById(Resource.Id.fullNameText);
+            phoneText = (TextInputLayout)FindViewById(Resource.Id.phoneText);
+            emailText = (TextInputLayout)FindViewById(Resource.Id.emailText);
+            passwordText = (TextInputLayout)FindViewById(Resource.Id.passwordText);
+            registerButton = (Button)FindViewById(Resource.Id.registerButton);
+            rootView = (CoordinatorLayout)FindViewById(Resource.Id.rootView);
+            registerButton.Click += RegisterButton_Click;
+        }
+
+        void InitialFirebase()
+        {
+            var app = FirebaseApp.InitializeApp(this);
+
+            if (app == null)
+            {
+                var options = new FirebaseOptions.Builder()
+                    .SetApplicationId("uberclone-6596f")
+                    .SetApiKey("AIzaSyAXqLpF_JVINHqrS74X_r6uMG8wW5EZlzs")
+                    .SetDatabaseUrl("https://uberclone-6596f.firebaseio.com")
+                    .SetStorageBucket("uberclone-6596f.appspot.com")
+                    .Build();
+
+                app = FirebaseApp.InitializeApp(this, options);
+                database = FirebaseDatabase.GetInstance(app);
+            }
+            else
+            {
+                database = FirebaseDatabase.GetInstance(app);
+            }
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            string fullname, phone, email, password;
+
+            fullname = fullNameText.EditText.Text;
+            phone = phoneText.EditText.Text;
+            email = emailText.EditText.Text;
+            password = passwordText.EditText.Text;
+
+            if(fullname.Length < 3)
+            {
+                Snackbar.Make(rootView, "Please enter a valid name", Snackbar.LengthShort).Show();
+                return;
+            }
+            else if(phone.Length < 9)
+            {
+                Snackbar.Make(rootView, "Please enter a valid number", Snackbar.LengthShort).Show();
+                return;
+            }
+            else if(!email.Contains("@"))
+            {
+                Snackbar.Make(rootView, "Please enter a valid email", Snackbar.LengthShort).Show();
+                return;
+            }
+            else if(password.Length < 8)
+            {
+                Snackbar.Make(rootView, "Please enter a password up to 8 characters", Snackbar.LengthShort).Show();
+                return;
+            }
+
+            RegisterUser(fullname, phone, email, password);
+        }
+
+        void RegisterUser(string name, string phone, string email, string password)
+        {
+            taskCompletionListener.Success += TaskCompletionListener_Success;
+            taskCompletionListener.Failure += TaskCompletionListener_Failure;
+            mAuth.CreateUserWithEmailAndPassword(email, password)
+                .AddOnSuccessListener(this, taskCompletionListener)
+                .AddOnFailureListener(this, taskCompletionListener);
+        }
+
+        private void TaskCompletionListener_Failure(object sender, EventArgs e)
+        {
+            Snackbar.Make(rootView, "User registration failed", Snackbar.LengthShort).Show();
+        }
+
+        private void TaskCompletionListener_Success(object sender, EventArgs e)
+        {
+            Snackbar.Make(rootView, "User registration was successful", Snackbar.LengthShort).Show();
         }
     }
 }
---
+++ UberCloneRFP/EventListeners/TaskCompletionListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Tasks;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UberCloneRFP.EventListeners
+{
+    public class TaskCompletionListener : Java.Lang.Object, IOnSuccessListener, IOnFailureListener
+    {
+        public event EventHandler Success;
+        public event EventHandler Failure;
+
+        public void OnFailure(Java.Lang.Exception e)
+        {
+            Failure.Invoke(this, new EventArgs());
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            Success.Invoke(this, new EventArgs());
+        }
+    }
+}
